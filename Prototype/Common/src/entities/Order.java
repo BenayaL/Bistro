6803843ff9ccdeb5f11ef844d278541b42a905cf
SteveTrabajo <natan00758@gmail.com>
2025-11-28 13:59:04 +0200

    Order class and class diagram modifications

--- Prototype/Common/src/entities/Order.java
+++ Prototype/Common/src/entities/Order.java
@@ -5,16 +5,16 @@ import java.time.LocalTime;
 
 public class Order {
 
-    private String OrderID;
-    private String confimationCode;
+    private int orderNumber;
+    private int confimationCode;
     private LocalDate orderDate;
     private LocalTime orderTime;
     private int dinersAmount;
     
-    //Constructor:
-    public Order(String orderID, String confimationCode, LocalDate orderDate,
+    // Constructor:
+    public Order(int orderNumber, int confimationCode, LocalDate orderDate,
 					   LocalTime orderTime, int dinersAmount) {
-		this.OrderID = orderID;
+		this.orderNumber = orderNumber;
 		this.confimationCode = confimationCode;
 		this.orderDate = orderDate;
 		this.orderTime = orderTime;
@@ -22,19 +22,19 @@ public class Order {
     }
     
     //Getters and Setters:
-    public String getOrderID() {
-		return OrderID;
+    public int getOrderID() {
+		return orderNumber;
 	}
     
-    public void setOrderID(String orderID) {
-		this.OrderID = orderID;
+    public void setOrderID(int orderNumber) {
+		this.orderNumber = orderNumber;
 	}
     	
-    public String getConfimationCode() {
+    public int getConfimationCode() {
 		return confimationCode;
 	}
     	
-    public void setConfimationCode(String confimationCode) {
+    public void setConfimationCode(int confimationCode) {
 		this.confimationCode = confimationCode;
 	}
     	
