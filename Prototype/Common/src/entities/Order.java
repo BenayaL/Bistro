176b15188224f24d42a9f4c334439bb0b32ea0b1
Benaya Leib <benayaleib@gmail.com>
2025-11-28 13:29:27 +0200

    Update Order.java

--- Prototype/Common/src/entities/Order.java
+++ Prototype/Common/src/entities/Order.java
@@ -12,55 +12,54 @@ public class Order {
     private int dinersAmount;
     
     //Constructor:
-    public Order(String reservationID, String confimationCode, LocalDate reservationDate,
-					   LocalTime reservationTime, int dinersAmount, boolean reserved, boolean inWaitList) {
-		this.OrderID = reservationID;
+    public Order(String orderID, String confimationCode, LocalDate orderDate,
+					   LocalTime orderTime, int dinersAmount) {
+		this.OrderID = orderID;
 		this.confimationCode = confimationCode;
-		this.orderDate = reservationDate;
-		this.orderTime = reservationTime;
+		this.orderDate = orderDate;
+		this.orderTime = orderTime;
 		this.dinersAmount = dinersAmount;
     }
     
-    //ReservationID getter and setter:
-    public String getReservationID() {
-        return OrderID;
-    }
+    //Getters and Setters:
+    public String getOrderID() {
+		return OrderID;
+	}
     
-    public void setReservationID(String reservationID) {
-        this.OrderID = reservationID;
-    }
-    
-    //ConfimationCode getter and setter:
+    public void setOrderID(String orderID) {
+		this.OrderID = orderID;
+	}
+    	
     public String getConfimationCode() {
-        return confimationCode;
-    }
-
+		return confimationCode;
+	}
+    	
     public void setConfimationCode(String confimationCode) {
-        this.confimationCode = confimationCode;
-    }
-    
-    //reservationDate getter and setter:
-    public LocalDate getReservationDate() {
-        return orderDate;
-    }
-
-    public void setReservationDate(LocalDate reservationDate) {
-        this.orderDate = reservationDate;
-    }
-    
-    //reservationTime getter and setter:
-    public LocalTime getReservationTime() {
-        return orderTime;
-    }
-
-    public void setReservationTime(LocalTime reservationTime) {
-        this.orderTime = reservationTime;
-    }
-    //dinersAmount getter and setter:
-    public int getDinersAmount() {
-        return dinersAmount;
-    }
-    public void setDinersAmount(int dinersAmount) {
-        this.dinersAmount = dinersAmount;
-    }
+		this.confimationCode = confimationCode;
+	}
+    	
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+		
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+		
+	public LocalTime getOrderTime() {
+		return orderTime;
+	}
+		
+	public void setOrderTime(LocalTime orderTime) {
+		this.orderTime = orderTime;
+	}
+		
+	public int getDinersAmount() {
+		return dinersAmount;
+	}
+		
+	public void setDinersAmount(int dinersAmount) {
+		this.dinersAmount = dinersAmount;
+	}
+		
 }
