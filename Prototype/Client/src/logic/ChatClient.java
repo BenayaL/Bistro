5e924b6d0760b33dc432ab0c160dbb9acc5c5036
Benaya Leib <benayaleib@gmail.com>
2025-11-29 10:40:21 +0200

    client changes
    -add files : client ctrl , chat client , client gui

---
+++ Prototype/Client/src/logic/ChatClient.java
@@ -0,0 +1,19 @@
+package logic;
+
+import ocsf.client.*;
+
+
+
+public class ChatClient extends AbstractClient{
+	
+	public ChatClient(String host, int port) {
+		super(host, port);
+	}
+
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
