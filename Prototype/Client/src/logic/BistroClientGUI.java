2c9c6debacddb1ea60eb9857752bfb3d6db43a34
Benaya Leib <benayaleib@gmail.com>
2025-12-01 14:49:58 +0200

    start work on clientGUI

--- Prototype/Client/src/logic/BistroClientGUI.java
+++ Prototype/Client/src/logic/BistroClientGUI.java
@@ -6,6 +6,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class BistroClientGUI extends Application{
+	
+	public static BistroClient client;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,7 +15,10 @@ public class BistroClientGUI extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		this.client=new BistroClient("localhost", 5555);
+		try {
+			client.openConnection();
+			
 	}  
    
 }
